42c9b7af38e430491b091349c0015b5c8d3bbec1
WildBabaloo <andphiltre@gmail.com>
2024-04-05 19:30:52 -0400

    Google nav bar package and implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -56,7 +57,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-
+    int selectedIndex = 1;
   void _incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
@@ -120,6 +121,71 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      bottomNavigationBar: Container(
+          color: Theme.of(context).colorScheme.secondaryContainer,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+            child: GNav(
+              backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
+              tabBackgroundColor: Theme.of(context).colorScheme.primary,
+              activeColor: Theme.of(context).colorScheme.onPrimary,
+              gap: 12,
+              padding: const EdgeInsets.all(20),
+              selectedIndex: 0,
+              onTabChange: (index) {
+                setState(() {
+                  selectedIndex = index;
+                  if (selectedIndex == 0) {
+                    /* 
+                    Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        pageBuilder: (context, animation, secondaryAnimation) =>
+                            pageProfil(), //remplacer par le nom de la  page,
+                      ),
+                    );
+                    */
+                  }
+                  if (selectedIndex == 1){
+                                        /* 
+                    Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        pageBuilder: (context, animation, secondaryAnimation) =>
+                            pageProfil(), //remplacer par le nom de la  page,
+                      ),
+                    );
+                    */
+                  }
+                  if (selectedIndex == 2) {
+                    /* 
+                    Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        pageBuilder: (context, animation, secondaryAnimation) =>
+                            pageProfil(), //remplacer par le nom de la  page,
+                      ),
+                    );
+                    */
+                  }
+                });
+              },
+              tabs: const [
+                GButton(
+                  icon: Icons.map_outlined,
+                  text: 'Map',
+                ),
+                GButton(
+                  icon: Icons.sunny,
+                  text: 'Weather',
+                ),
+                GButton(
+                  icon: Icons.account_circle,
+                  text: 'Profile',
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
