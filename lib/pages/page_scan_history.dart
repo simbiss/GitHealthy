212d1278e72a5c8c80a592e01ed51c09feb6b24a
Simon <simonbiss20@gmail.com>
2024-04-06 01:56:26 -0400

    lister l'historique de la bd

--- lib/pages/page_scan_history.dart
+++ lib/pages/page_scan_history.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
 
 // A mock class to represent a scanned item.
 class ScannedItem {
@@ -8,15 +12,16 @@ class ScannedItem {
   final String timeScanned;
 
   ScannedItem(this.name, this.brand, this.quality, this.timeScanned);
-}
 
-// A list to simulate a history of scanned items.
-final List<ScannedItem> scannedHistory = [
-  ScannedItem('Monster Energy Zero Sugar', 'Monster energy', 'Médiocre',
-      'À l\'instant'),
-  ScannedItem('Stax Salt & Vinegar', 'Lay\'s', 'Mauvais', 'il y a 1 heure'),
-  
-];
+  factory ScannedItem.fromJson(Map<String, dynamic> json) {
+    return ScannedItem(
+      json['name'],
+      json['brand'],
+      json['quality'],
+      json['timeScanned'],
+    );
+  }
+}
 
 class HistoryPage extends StatefulWidget {
   @override
@@ -24,27 +29,53 @@ class HistoryPage extends StatefulWidget {
 }
 
 class _HistoryPageState extends State<HistoryPage> {
+  List<ScannedItem> scannedHistory = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchScannedItems();
+  }
+
+  Future<void> _fetchScannedItems() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final String? username = prefs.getString('userName');
+
+    if (username == null) {
+      print("Username not found");
+      return;
+    }
+
+    final Uri uri = Uri.parse('http://v34l.com:8080/api/$username/barcodes');
+
+    try {
+      final response = await http.get(uri);
+      if (response.statusCode == 200) {
+        final List<dynamic> fetchedItems = json.decode(response.body);
+        setState(() {
+          scannedHistory = fetchedItems
+              .map((dynamic item) => ScannedItem.fromJson(item))
+              .toList();
+        });
+      } else {
+        throw Exception('Failed to load history');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Historique'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.info_outline),
-            onPressed: () {
-              // Handle info action
-            },
-          ),
-        ],
       ),
       body: ListView.builder(
         itemCount: scannedHistory.length,
         itemBuilder: (context, index) {
           final item = scannedHistory[index];
           return ListTile(
-            // leading: Image.network(
-                // 'marihacks7/images/oreo.png'), // Placeholder for item image
             title: Text(item.name),
             subtitle: Text(item.brand),
             trailing: Column(
