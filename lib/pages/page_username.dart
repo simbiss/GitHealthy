56fd6db7859b2db8f2f8f77aa86eb7003385988f
Simon <simonbiss20@gmail.com>
2024-04-06 00:27:20 -0400

    ajout user dans la bd

--- lib/pages/page_username.dart
+++ lib/pages/page_username.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:marihacks7/pages/page_camera_scanner.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class WelcomePage extends StatefulWidget {
   @override
@@ -15,6 +17,30 @@ class _WelcomePageState extends State<WelcomePage> {
   void initState() {
     super.initState();
     _checkIfUserIsNew();
+  }
+
+  Future<void> _createUser(String username) async {
+    Uri uri = Uri.parse('http://v34l.com:8080/api/add');
+    try {
+      final response = await http.post(
+        uri,
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode({'userid': username}),
+      );
+
+      if (response.statusCode == 200) {
+        // Si le serveur renvoie une réponse réussie, naviguez vers BarcodeScanPage
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => BarcodeScanPage()),
+        );
+      } else {
+        // Gérez les erreurs de requête ici
+        throw Exception('Failed to create user');
+      }
+    } catch (e) {
+      // Gérez les exceptions de connexion ici
+      print(e.toString());
+    }
   }
 
   Future<void> _checkIfUserIsNew() async {
@@ -35,9 +61,7 @@ class _WelcomePageState extends State<WelcomePage> {
     if (_nameController.text.isNotEmpty) {
       final SharedPreferences prefs = await SharedPreferences.getInstance();
       await prefs.setString('userName', _nameController.text.trim());
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => BarcodeScanPage()),
-      );
+      await _createUser(_nameController.text.trim());
     }
   }
 
